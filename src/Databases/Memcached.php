f9fa2339b56017d228a2fae8ed6e0c94d48aeb76
= <=>
2022-10-06 23:56:56 +0300

    Memcached added

--- src/Databases/Memcached.php
+++ src/Databases/Memcached.php
@@ -4,5 +4,45 @@ namespace Src\Databases;
 
 class Memcached
 {
+    private \Memcached $memcached;
+    private string $prefix = '';
 
+    public function __construct(array $services = ['127.0.0.1'])
+    {
+        $this->memcached = new \Memcached();
+
+        foreach ($services as $service)
+            $this->memcached->addServer($service, 11211);
+    }
+
+    public function get(array|string $keys)
+    {
+        if (is_array($keys))
+            return $this->memcached->getMulti(
+                array_map(
+                    fn($key) => $this->prefix . $key,
+                    $keys
+                )
+            );
+
+        return $this->memcached->get($this->prefix . $keys);
+    }
+
+    public function set(string $key, mixed $data): void
+    {
+        $this->memcached->set($this->prefix . $key, [
+            'data' => $data,
+            'timestamp' => microtime(true)
+        ]);
+    }
+
+    public function flushAll(): void
+    {
+        $this->memcached->flush();
+    }
+
+    public function setPrefix($prefix): void
+    {
+        $this->prefix = $prefix . '_';
+    }
 }
---
+++ tests/Databases/Memcached.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Databases;
+
+use PHPUnit\Framework\TestCase;
+
+class Memcached extends TestCase
+{
+    /*
+     * This test need Memcached to be started
+     */
+    public function setUp(): void
+    {
+        $this->memcached = new \Src\Databases\Memcached();
+
+        $this->memcached->flushAll();
+    }
+
+    public function tearDown(): void
+    {
+        $this->memcached->flushAll();
+
+        parent::tearDown();
+    }
+
+    /** @test */
+    public function set_one_key_and_get()
+    {
+        $this->memcached->set('test', ['one', 'two', 'three']);
+
+        $data = $this->memcached->get('test');
+
+        $this->assertEquals(['one', 'two', 'three'], $data['data']);
+        $this->assertIsFloat($data['timestamp']);
+    }
+
+    /** @test */
+    public function set_keys_and_get_all_with_prefix()
+    {
+        $this->memcached->setPrefix('prefix');
+
+        $this->memcached->set('test_one', ['one', 'two', 'three']);
+        $this->memcached->set('test_two', ['four', 'five', 'six']);
+        $this->memcached->set('test_three', 'seven');
+
+        $data = $this->memcached->get(['test_one', 'test_two', 'test_three']);
+
+        $this->assertEquals(['one', 'two', 'three'], $data['prefix_test_one']['data']);
+        $this->assertEquals(['four', 'five', 'six'], $data['prefix_test_two']['data']);
+        $this->assertEquals('seven', $data['prefix_test_three']['data']);
+        $this->assertIsFloat($data['prefix_test_one']['timestamp']);
+
+        $this->memcached->setPrefix('');
+
+        $data = $this->memcached->get(['test_one', 'test_two', 'test_three']);
+
+        $this->assertEmpty($data);
+    }
+}

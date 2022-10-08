3438a82de34031272f14902f8ea7267d0ce63875
= <=>
2022-10-08 11:17:52 +0300

    Refactor Memcached

--- src/Databases/Memcached.php
+++ src/Databases/Memcached.php
@@ -2,43 +2,51 @@
 
 namespace Src\Databases;
 
-class Memcached
+class Memcached extends \Memcached
 {
-    private \Memcached $memcached;
     private string $prefix = '';
 
     public function __construct(array $services = ['127.0.0.1'])
     {
-        $this->memcached = new \Memcached();
+        parent::__construct();
 
         foreach ($services as $service)
-            $this->memcached->addServer($service, 11211);
+            $this->addServer($service, 11211);
     }
 
-    public function get(array|string $keys)
+    public function getData(array|string $keys): mixed
     {
         if (is_array($keys))
-            return $this->memcached->getMulti(
+            return $this->getMulti(
                 array_map(
                     fn($key) => $this->prefix . $key,
                     $keys
                 )
             );
 
-        return $this->memcached->get($this->prefix . $keys);
+        return $this->get($this->prefix . $keys);
     }
 
-    public function set(string $key, mixed $data): void
+    /**
+     * (PECL memcached &gt;= 0.1.0)<br/>
+     * Store an item
+     * @link https://php.net/manual/en/memcached.set.php
+     * @param string $key <p>
+     * The key under which to store the value.
+     * </p>
+     * @param mixed $value <p>
+     * The value to store.
+     * </p>
+     * @param int $expiration [optional] <p>
+     * The expiration time, defaults to 0. See Expiration Times for more info.
+     * </p>
+     * @param int $udf_flags [optional]
+     * @return bool <b>TRUE</b> on success or <b>FALSE</b> on failure.
+     * Use <b>Memcached::getResultCode</b> if necessary.
+     */
+    public function set($key, $value, $expiration = 0, $udf_flags = 0): bool
     {
-        $this->memcached->set($this->prefix . $key, [
-            'data' => $data,
-            'timestamp' => microtime(true)
-        ]);
-    }
-
-    public function flushAll(): void
-    {
-        $this->memcached->flush();
+        return parent::set($this->prefix . $key, ['data' => $value, 'timestamp' => microtime(true)], $expiration);
     }
 
     public function setPrefix($prefix): void
--- tests/Databases/Memcached.php
+++ tests/Databases/Memcached.php
@@ -6,56 +6,17 @@ use PHPUnit\Framework\TestCase;
 
 class Memcached extends TestCase
 {
-    /*
-     * This test need Memcached to be started
-     * sudo /etc/init.d/memcached restart
-     * sudo systemctl restart memcached
-     */
-    public function setUp(): void
-    {
-        $this->memcached = new \Src\Databases\Memcached();
-
-        $this->memcached->flushAll();
-    }
-
-    public function tearDown(): void
-    {
-        $this->memcached->flushAll();
-
-        parent::tearDown();
-    }
-
     /** @test */
-    public function set_one_key_and_get()
+    public function test()
     {
-        $this->memcached->set('test', ['one', 'two', 'three']);
+        $data = ['one', 'two', 'three'];
 
-        $data = $this->memcached->get('test');
+        $stub = $this->createMock(\Src\Databases\Memcached::class);
 
-        $this->assertEquals(['one', 'two', 'three'], $data['data']);
-        $this->assertIsFloat($data['timestamp']);
-    }
+        $stub->expects($this->once())
+            ->method('set')
+            ->with('test', $data);
 
-    /** @test */
-    public function set_keys_and_get_all_with_prefix()
-    {
-        $this->memcached->setPrefix('prefix');
-
-        $this->memcached->set('test_one', ['one', 'two', 'three']);
-        $this->memcached->set('test_two', ['four', 'five', 'six']);
-        $this->memcached->set('test_three', 'seven');
-
-        $data = $this->memcached->get(['test_one', 'test_two', 'test_three']);
-
-        $this->assertEquals(['one', 'two', 'three'], $data['prefix_test_one']['data']);
-        $this->assertEquals(['four', 'five', 'six'], $data['prefix_test_two']['data']);
-        $this->assertEquals('seven', $data['prefix_test_three']['data']);
-        $this->assertIsFloat($data['prefix_test_one']['timestamp']);
-
-        $this->memcached->setPrefix('');
-
-        $data = $this->memcached->get(['test_one', 'test_two', 'test_three']);
-
-        $this->assertEmpty($data);
+        $stub->set('test', $data);
     }
 }

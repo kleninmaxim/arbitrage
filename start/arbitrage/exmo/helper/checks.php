16799ee498ce9a8a849bd3eae3497627a061a7f0
= <=>
2022-11-18 15:35:58 +0300

    Update checks

--- start/arbitrage/exmo/helper/checks.php
+++ start/arbitrage/exmo/helper/checks.php
@@ -5,6 +5,7 @@ require_once dirname(__DIR__, 4) . '/index.php';
 $memcached = new Memcached();
 if ($memcached->addServer('127.0.0.1', 11211)) {
     echo '[' . date('Y-m-d H:i:s') . '] [OK] Memcached' . PHP_EOL;
+    $memcached->quit();
 } else {
     echo '[' . date('Y-m-d H:i:s') . '] [FAIL] Memcached' . PHP_EOL;
 }
@@ -12,6 +13,7 @@ if ($memcached->addServer('127.0.0.1', 11211)) {
 $redis = new Redis();
 if ($redis->connect('127.0.0.1')) {
     echo '[' . date('Y-m-d H:i:s') . '] [OK] Redis' . PHP_EOL;
+    $redis->close();
 } else {
     echo '[' . date('Y-m-d H:i:s') . '] [FAIL] Redis' . PHP_EOL;
 }
